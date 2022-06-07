2de8c56903696bb2a1b6e2607e861f3496e55e55
naivvve <1532619822@qq.com>
2022-06-07 21:40:10 +0800

    Message

--- web/my-charts.dart
+++ web/my-charts.dart
@@ -1,20 +1,16 @@
-// Dart JS示例
-// AntV可视化组件（https://antv.antfin.com/zh-cn/g2/3.x/demo/pie/color-rose.html#）
-// Dart JS文档（https://api.dart.cn/stable/2.16.2/dart-js/dart-js-library.html）
-
 import 'dart:html';
 import 'dart:js';
 
 void main(List<String> args) {
   DivElement dartStatus = querySelector('#dart-status') as DivElement;
-  dartStatus.text = 'Dart is running!';
+  dartStatus.text = 'Here is how much people like the menu！';
 
   var myData = JsObject.jsify([
-    {'year': '2001', 'population': 2},
-    {'year': '2002', 'population': 4},
-    {'year': '2003', 'population': 6},
-    {'year': '2004', 'population': 8},
-    {'year': '2005', 'population': 10},
+    {'menu': '板栗炖鸡', 'number': 6},
+    {'menu': '虾仁滑蛋', 'number': 2},
+    {'menu': '番茄牛腩', 'number': 4},
+    {'menu': '豉汁排骨', 'number': 8},
+    {'menu': '芝士虾球', 'number': 10},
   ]);
 
   var chart = context['chart'];
